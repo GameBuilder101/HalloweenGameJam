efaf5ba69422f8b5c685e6850493bab54f95b4f6
GameBuilder101 <gamebuilder101@gmail.com>
2023-10-28 17:35:21 -0400

    Fixed window sizing

--- tasks/spelling/SpellingTask.cs
+++ tasks/spelling/SpellingTask.cs
@@ -3,23 +3,24 @@ using Godot;
 [GlobalClass]
 public partial class SpellingTask : Task
 {
+    [Export]
+    private Button _checkButton;
+    [Export]
+    private Image _image;
+    [Export]
+    private LineEdit _characterEdit;
+    [Export]
+    private Label _leftLabel;
+    [Export]
+    private Label _rightLabel;
+
     public override void _Ready()
     {
         base._Ready();
-        GD.Print(_currentDifficultyIndex); // Use the _currentDifficultyIndex to get the difficulty. 0 is the lowest and 4 is the highest.
-        RandomNum.Next(5); // Use this for random numbers
     }
 
     public override void _Process(double delta)
     {
         base._Process(delta);
-        if (true)
-        {
-            Pass();
-        }
-        else
-        {
-            Fail();
-        }
     }
 }
