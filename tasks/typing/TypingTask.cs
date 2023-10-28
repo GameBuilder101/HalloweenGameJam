201542390cd105bf85f9820cd75ae98bb8658334
GameBuilder101 <gamebuilder101@gmail.com>
2023-10-28 16:52:26 -0400

    Started adding templates

---
+++ tasks/typing/TypingTask.cs
@@ -0,0 +1,25 @@
+using Godot;
+
+[GlobalClass]
+public partial class TypingTask : Task
+{
+    public override void _Ready()
+    {
+        base._Ready();
+        GD.Print(_currentDifficultyIndex); // Use the _currentDifficultyIndex to get the difficulty. 0 is the lowest and 4 is the highest.
+        RandomNum.Next(5); // Use this for random numbers
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        if (true)
+        {
+            Pass();
+        }
+        else
+        {
+            Fail();
+        }
+    }
+}
