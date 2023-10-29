4c59cb583b9299e59631ebe03a0e943c30cc8aa2
Lyx Huston <lyx@hustonfam.net>
2023-10-28 21:57:28 -0400

    math task basically done
    only need to add more equations

--- tasks/math/MathTask.cs
+++ tasks/math/MathTask.cs
@@ -3,22 +3,70 @@ using Godot;
 [GlobalClass]
 public partial class MathTask : Task
 {
-	public override void _Ready()
-	{
-		base._Ready();
-		GD.Print(_currentDifficultyIndex); // Use the _currentDifficultyIndex to get the difficulty. 0 is the lowest and 4 is the highest.
-		RandomNum.Next(5); // Use this for random numbers
-    }
+	[Export]
+	private LaTeX display;
+	[Export]
+	private TextEdit textInput;
 
-	public override void _Process(double delta)
-	{
-		base._Process(delta);
-		if (true)
-		{
-			Pass();
+	private int answer;
+
+	public override void SetDifficulty(int index) {
+		base.SetDifficulty(index);
+
+		int randFactor = RandomNum.Next(9) + 1;
+		string latexString = "{0}";
+		answer = randFactor;
+
+		switch (CurrentDifficulty.Score) {
+			case 10: {
+				switch (RandomNum.Next(4)) {
+					case 0: {
+						latexString = "\\frac[2520][{0}]";
+						answer = 2520 / randFactor;
+						break;
+					}
+					case 1: {
+						latexString = "{0}^2";
+						answer = randFactor * randFactor;
+						break;
+					}
+					case 2: {
+						latexString = "1^{0}";
+						answer = 1;
+						break;
+					}
+					case 3: {
+						latexString = "\\sqrt[{0}^2]";
+						answer = randFactor;
+						break;
+					}
+				}
+				break;
+			}
+			case 30: {
+				switch (RandomNum.Next(4)) {
+					case 0: {
+						latexString = "\\frac{}{}";
+						break;
+					}
+				}
+				break;
+			}
 		}
-		else
-		{
+
+		string formattedString = string.Format(latexString, randFactor).Replace(
+				'[', '{').Replace(']', '}');
+		display.LatexExpression = formattedString;
+		display.Render();
+	}
+	
+	private void Submit() {
+		int result;
+		bool success = int.TryParse(textInput.Text, out result);
+		if (!success) return;
+		if (result == answer) {
+			Pass();
+		} else {
 			Fail();
 		}
 	}
